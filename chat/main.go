52c1ce9c3ac2533369ed9c14e202bb3f5e876e20
Ferdinandus Richard <reeechart@gmail.com>
2019-11-22 10:05:04 +0700

    add sample main program

--- chat/main.go
+++ chat/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+var (
+	scanner *bufio.Scanner
+)
 
 func main() {
-	fmt.Println("hello")
+	initiateForoomSession()
+
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+
+	<-interruptChan
+	fmt.Println()
+	fmt.Println("Exited foroom")
+}
+
+func initiateForoomSession() {
+	fmt.Println("Welcome to Foroom")
+	fmt.Print("Please enter room name: ")
+	scanner = bufio.NewScanner(os.Stdin)
+	roomName := receiveRoomName()
+	fmt.Printf("Welcome to room %s\n", roomName)
+	fmt.Printf("Start chatting in room %s!\n", roomName)
+}
+
+func receiveRoomName() string {
+	scanner.Scan()
+	return scanner.Text()
 }
